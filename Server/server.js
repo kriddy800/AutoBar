64a23685d078e97f5152e92293a78297ed9f2020
kDupuis <kristopherdupuis@gmail.com>
2016-08-27 11:33:35 -0400

    Change pin

--- Server/server.js
+++ Server/server.js
@@ -9,9 +9,9 @@ app.get('/', function(req, res) {})
 app.listen(9091, function() {
     console.log("server running on 9091");
 
-    gpio.open(4, "output", function(err) {
-        gpio.write(4, 0, function() {
-            gpio.close(4);
+    gpio.open(5, "output", function(err) {
+        gpio.write(5, 0, function() {
+            gpio.close(5);
         });
     });
 });
