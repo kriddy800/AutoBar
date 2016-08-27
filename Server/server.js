90fc4d242856a5efd6c57677fba086ff3e738c6c
kDupuis <kristopherdupuis@gmail.com>
2016-08-27 10:50:18 -0400

    Test GPIO

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 
 var app = express();
+var gpio = require("pi-gpio");
 
 app.use(express.static('../App'));
-app.get('/', function(req, res) {
-})
+app.get('/', function(req, res) {})
 
-app.listen(9091);
+app.listen(9091, function() {
+    console.log("server running on 9091");
+
+    gpio.open(4, "output", function(err) {
+        gpio.write(4, 0, function() {
+            gpio.close(4);
+        });
+    });
+});
